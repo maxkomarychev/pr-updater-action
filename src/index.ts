74b74fcb5230e89ab098cb86afb5b216d2b49611
Max Komarychev <maxkomarychev@gmail.com>
2019-10-22 10:23:57 +0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,29 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import chalk from 'chalk'
 
 const token = core.getInput('token')
 const client = new github.GitHub(token)
 
-client.pulls
-    .list({
+console.log('payload', github.context.payload)
+console.log('ref', github.context.ref)
+
+async function main() {
+    const pullsResponse = await client.pulls.list({
         ...github.context.repo,
         base: 'master',
         state: 'open',
     })
-    .then((prs) => {
-        // console.log('prs!', prs)
-        const array = prs.data
-        return Promise.all(
-            array.map((pr) => {
-                console.log('updating pr branch #', pr.number)
-                return client.pulls.updateBranch({
-                    ...github.context.repo,
-                    pull_number: pr.number,
-                })
-            }),
-        ).then((responses) => {
-            console.log('responses!', responses)
-        })
-    })
-    .catch((error) => {
-        console.log('can not fetch all prs', error)
-        core.setFailed(error.message)
-    })
+    const prs = pullsResponse.data
+    await Promise.all(
+        prs.map((pr) => {
+            // console.log(chalk.yellow("Updating pull request"))
+            client.pulls.updateBranch({
+                ...github.context.repo,
+                pull_number: pr.number,
+            })
+        }),
+    )
+}
+
+main()
