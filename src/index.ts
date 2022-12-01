3e19fa1e3e21a87d33939ce71aa84ed02ef6c3ca
Wiggins <wiggins@twentyideas.com>
2022-12-01 07:02:56 -0800

    provide an option to exclude draft PRs from being updated

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 
 const token = core.getInput('token')
+const exclude_drafts = core.getInput('exclude_drafts').toLowerCase() === "true"
 const client = new github.GitHub(token)
 
 async function main() {
@@ -14,11 +15,14 @@ async function main() {
     const prs = pullsResponse.data
     await Promise.all(
         prs.map((pr) => {
+            if (exclude_drafts && pr.draft) 
+                return null
+
             client.pulls.updateBranch({
                 ...github.context.repo,
                 pull_number: pr.number,
             })
-        }),
+        }).filter(p => p),
     )
 }
 
