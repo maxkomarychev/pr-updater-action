49f20b10474a8120bfa6f831c6ea92e9f425e29f
kendelsere <ken@twentyideas.com>
2022-12-22 12:28:27 -0800

    Still not seeing logs. Maybe it's happening earlier?

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ const exclude_drafts = core.getInput('exclude_drafts').toLowerCase() === "true"
 const client = new github.GitHub(token)
 
 async function main() {
+    core.info("Starting update prs")
     const baseBranch = github.context.payload.ref
     const pullsResponse = await client.pulls.list({
         ...github.context.repo,
@@ -13,9 +14,11 @@ async function main() {
         state: 'open',
     })
     let prs = pullsResponse.data
-        .filter(pr => !exclude_drafts || !pr.draft)
-    prs.forEach((pr) => {
-        core.info(JSON.stringify(pr))
+    core.info(JSON.stringify(prs))
+
+    prs.filter(pr => !exclude_drafts || !pr.draft)
+        .forEach((pr) => {
+            core.info(JSON.stringify(pr))
             try {
                 await client.pulls.updateBranch({
                     ...github.context.repo,
